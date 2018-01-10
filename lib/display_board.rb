d6c00a5a00254c4bae0f4214000a7a6ba4e7a189
David McGrann <dpmcgrann@gmail.com>
2018-01-10 04:43:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,12 +6,13 @@ def display_board
   puts rows[3]
   puts rows[4]
 end
+
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
-  puts rows[0]
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts rows[1]
-  puts rows[2]
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts rows[3]
-  puts rows[4]
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
