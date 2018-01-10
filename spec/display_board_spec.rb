d9cc7966d3a9485d85b1db3efda850f9b82f3dae
David McGrann <dpmcgrann@gmail.com>
2018-01-10 04:49:57 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -182,7 +182,7 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
 
-      expect(rows[0]).to eq(" O | ) | O ")
+      expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
