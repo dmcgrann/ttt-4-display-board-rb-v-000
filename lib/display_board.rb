a7d011e4f8078bb4111852269cd29e0bdcefcfee
David McGrann <dpmcgrann@gmail.com>
2018-01-10 04:39:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,3 +6,12 @@ def display_board
   puts rows[3]
   puts rows[4]
 end
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+  puts rows[0]
+  puts rows[1]
+  puts rows[2]
+  puts rows[3]
+  puts rows[4]
+end
